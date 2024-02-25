491de1dae0a6bda99f1d6f7f5c0e15cd25c370d5
Steven Tabango <steven.tabango@gmail.com>
2024-02-25 10:02:58 -0500

    Collision detection && game over

---
+++ README.md
@@ -0,0 +1,31 @@
+# Retro Game Project
+
+Welcome to my Retro Game project! This endeavor is a personal tribute to the classic video games that many of us grew up loving, now reimagined using pure JavaScript, Object-Oriented Programming (OOP), and the HTML canvas element.
+
+## Live Version
+
+Experience the game firsthand by visiting the live version here: [Play Retro Game](https://retro-game-js-zbangosystems.vercel.app/).
+
+This web adaptation brings the beloved classic gameplay directly to your browser, allowing you to jump into the fun without any downloads or installations. Whether you're reminiscing about the past or discovering these gems for the first time, I hope you enjoy the mix of nostalgia and contemporary technology.
+
+## Instructions
+
+To ensure the best gaming experience, here are the controls for the game:
+
+- **Spacebar:** Fire your weapon.
+- **Left Arrow:** Move left.
+- **Right Arrow:** Move right.
+- **R:** Restart the game.
+
+These controls are designed to be intuitive and easy to use, allowing you to fully immerse yourself in the game without having to worry about complicated command schemes.
+
+
+## Feedback and Suggestions
+
+I highly value your feedback and suggestions! If you have any ideas on how to improve the game, encounter any bugs, or simply want to share your thoughts, please feel free to contact me. You can reach out to me via email at:
+
+- **Email:** [steven.tabango@gmail.com](mailto:steven.tabango@gmail.com)
+
+Your input is crucial in my quest to continually enhance the Retro Game project and ensure it provides the best possible experience.
+
+Thank you for your interest and support. Happy gaming!
--- script.js
+++ script.js
@@ -6,6 +6,7 @@ class Player {
         this.x = this.game.width * 0.5 - this.width * 0.5;
         this.y = this.game.height - this.height;
         this.speed = 5;
+        this.lives = 3;
     }
     draw(context) {
         context.fillRect(this.x, this.y, this.width, this.height);
@@ -19,6 +20,11 @@ class Player {
     shoot() {
         const projectile = this.game.getProjectile();
         if (projectile) projectile.start(this.x + this.width * 0.5, this.y);
+    }
+    restart(){
+        this.x = this.game.width * 0.5 - this.width * 0.5;
+        this.y = this.game.height - this.height;
+        this.lives = 3;
     }
 }
 
@@ -61,6 +67,7 @@ class Enemy {
         this.y = 0;
         this.positionX = positionX;
         this.positionY = positionY;
+        this.markedForDeletion = false;
     }
     draw(context) {
         context.strokeRect(this.x, this.y, this.width, this.height);
@@ -68,6 +75,27 @@ class Enemy {
     update(x, y) {
         this.x = x + this.positionX;
         this.y = y + this.positionY;
+        //collision
+        this.game.projectilesPool.forEach(projectile => {
+            if (!projectile.free && this.game.checkCollision(this, projectile)) {
+                this.markedForDeletion = true;
+                projectile.reset();
+                if(!this.game.gameOver)
+                    this.game.score++;
+            }
+        })
+        if(this.game.checkCollision(this, this.game.player)){
+            this.markedForDeletion = true;
+            if(!this.game.gameOver && this.game.score > 0) this.game.score--;
+            this.game.player.lives--;
+            if(this.game.player.lives < 1) this.game.gameOver = true;
+
+        }
+        // game over
+        if (this.y + this.height > this.game.height) {
+            this.game.gameOver = true;
+            this.markedForDeletion = true;
+        }
     }
 }
 
@@ -81,6 +109,7 @@ class Wave {
         this.speedX = 3;
         this.speedY = 0;
         this.enemies = [];
+        this.nextWaveTrigger = false;
         this.create();
     }
     render(context) {
@@ -96,6 +125,7 @@ class Wave {
             enemy.update(this.x, this.y);
             enemy.draw(context);
         })
+        this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
     }
     create() {
         for (let y = 0; y < this.game.rows; y++) {
@@ -117,15 +147,20 @@ class Game {
         this.projectilesPool = [];
         this.numberOfProjectiles = 10;
         this.createProjectiles();
-        this.columns = 3;
-        this.rows = 3;
+        this.columns = 5;
+        this.rows = 5;
         this.enemySize = 60;
         this.waves = [];
         this.waves.push(new Wave(this));
+        this.waveCount = 1;
+        this.score = 0;
+        this.gameOver = false;
         // event listeners
         window.addEventListener('keydown', (e) => {
             if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
-            if (e.key === "1") this.player.shoot();
+            // space bar
+            if (e.key === " ") this.player.shoot();
+
         })
         window.addEventListener('keyup', (e) => {
             const index = this.keys.indexOf(e.key);
@@ -133,6 +168,7 @@ class Game {
         })
     }
     render(context) {
+        this.drawStatusText(context);
         this.player.draw(context);
         this.player.update();
         this.projectilesPool.forEach(projectile => {
@@ -141,6 +177,12 @@ class Game {
         })
         this.waves.forEach(wave => {
             wave.render(context);
+            if(wave.enemies.length < 1 && !wave.nextWaveTrigger && !this.gameOver){
+                this.waveCount++;
+                this.newWave();
+                wave.nextWaveTrigger = true;
+                this.player.lives++;
+            }
         })
     }
     createProjectiles() {
@@ -153,7 +195,40 @@ class Game {
             if (this.projectilesPool[i].free) return this.projectilesPool[i];
         }
     }
-    
+    // collision
+    checkCollision(a, b) {
+        return (
+            a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y
+        )
+    }
+    drawStatusText(context) {
+        context.save();
+        context.shadowOffsetX = 2;
+        context.shadowOffsetY = 2;
+        context.shadowColor = "black";
+        context.fillText("Score: " + this.score, 0, 60);
+        context.fillText("Wave: " + this.waveCount, 0, 100);
+        for(let i = 0; i < this.player.lives; i++){
+            context.fillRect(10 * i, 120, 5, 20);
+        }
+
+        if (this.gameOver) {
+            context.textAlign = "center";
+            context.font = "100px Impact";
+            context.fillText("Game Over", this.width * 0.5, this.height * 0.5);
+            context.font = "20px Impact";
+            context.fillText("Press R to restart", this.width * 0.5, this.height * 0.5 + 30);
+        }
+        context.restore();
+    }
+    newWave() {
+        if(Math.random() < 0.5 && this.columns * this.enemySize < this.width * 0.8) this.columns++;
+        else if(this.rows * this.enemySize < this.height * 0.6) this.rows++;
+        this.waves.push(new Wave(this));
+    }
 }
 
 window.addEventListener('load', function () {
@@ -165,6 +240,7 @@ window.addEventListener('load', function () {
     ctx.fillStyle = 'white';
     ctx.strokeStyle = "white";
     ctx.lineWidth = 5;
+    ctx.font = '30px Impact';
     const game = new Game(canvas);
 
 
